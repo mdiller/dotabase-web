2b6e712dee612e7e04bbd54db155b28cb2b80f91
Malcolm Diller <dillerm@oregonstate.edu>
2018-03-17 22:08:31 -0700

    added background image to homepage

--- src/components/pages/HomePage.jsx
+++ src/components/pages/HomePage.jsx
@@ -1,9 +1,21 @@
 import React, { Component } from 'react';
 import VoiceSelect from '../VoiceSelect.jsx';
 import dotabase from '../../Dotabase.js';
-import '../../styles/responses.css';
+import '../../styles/homepage.css';
 
 class HomePage extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			loadingscreen: null
+		};
+	}
+	componentDidMount() {
+		const self = this;
+		dotabase.query("SELECT * FROM loadingscreens ORDER BY RANDOM() LIMIT 1").then(response => {
+			self.setState({ loadingscreen: response.rows[0] });
+		});
+	}
 	render() {
 		return (
 			<div>
@@ -12,6 +24,9 @@ class HomePage extends Component {
 						Dotabase
 					</h1>
 				</div>
+				<img 
+					id="backgroundimage" 
+					src={this.state.loadingscreen ? dotabase.vpk_path + this.state.loadingscreen.image : ""} />
 			</div>
 		);
 	}
